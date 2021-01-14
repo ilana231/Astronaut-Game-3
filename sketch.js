d172460b82b379a6676e0518c388cff8e689f185
ilana231 <66657835+ilana231@users.noreply.github.com>
2021-01-13 20:06:45 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -4,12 +4,20 @@ var alien, alien_ufo
 var backdrop, background_moving
 var backdroptwo, backdrop_moving
 
+var obstaclesGroup, obstacle1,obstacle2,obstacle3,obstacle4,obstacle5
+
+
 
 function preload() {
 astronaut_floating = loadImage("astronaut floating 2.png");
 alien_ufo = loadImage("flying saucer.png");
 background_moving= loadImage("space img.jpg");
 backdrop_moving= loadImage("space 3.jpg");
+obstacle1= loadImage("asteroid.png");
+obstacle2= loadImage("rock.png");
+obstacle3= loadImage("rock 2.png");
+obstacle4= loadImage("little alien.png");
+obstacle5= loadImage("satellite.png");
 
 }
 
@@ -34,7 +42,7 @@ function setup() {
 
 
 function draw() {
-  background(255,255,255);
+  background("#060E1D");
   backdrop.velocityX = -5;
   backdroptwo.velocityX = -5; 
 
@@ -42,14 +50,38 @@ function draw() {
     //backdrop.x = backdrop.width/2
   //}
 
+  if(backdroptwo.x < 0) {
+    backdrop.x = backdrop.width/2;
+    backdroptwo.x = backdrop.width * 2.5;
+  };
+
   if(keyDown(UP_ARROW)){
     astronaut.y = astronaut.y - 5
+    alien.y = alien.y - 3
   }
 
   if(keyDown(DOWN_ARROW)) {
     astronaut.y = astronaut.y + 5
+    alien.y = alien.y + 3
   }
-  
+
+
+  spawnObstacles();
 
   drawSprites();
+}
+
+function spawnObstacles(){
+  var rand = random (0,displayHeight);
+if (frameCount %60 === 0) {
+  obstacle1 = createSprite(displayWidth + 10, rand, 20,20);
+  obstacle1.velocityX = -5;
+  obstacle1.lifetime = displayWidth/5;
+
+}
+
+//obstacle2 = createSprite(displayWidth + 10, rand, 20,20);
+//var randImg = Math.round(random(1,5));
+
+
 }
